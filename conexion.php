dca2e0a4ce4f8449a1a45230f13d57aa1cf0fe78
IYEM-TI <iyem.tecnologias@gmail.com>
2025-01-04 23:42:40 -0600

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,8 +1,8 @@
 <?php
   $host = "u3r5w4ayhxzdrw87.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
   $user = "s4qpw6q102xue4d8";
-  $pass = "	do5ahltm6g0jnnob";
-  $bd = "	j65m8y5wb6b076bv";
+  $pass = "do5ahltm6g0jnnob";
+  $bd = "j65m8y5wb6b076bv";
 
   $conectar = mysqli_connect($host,$user,$pass,$bd);
 
